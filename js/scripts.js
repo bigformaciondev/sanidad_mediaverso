7a429ae8c1efa692f65b31967ff600e4eb207a28
[srfrutos] <[sergiodfgl@gmail.com]>
2025-02-25 15:00:08 +0100

    cambio error redireccion (faltan)

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,15 @@ document.addEventListener("DOMContentLoaded", function () {
     loadMenusAndSubmenus();
   });
 function irASeccion(id) {
-    window.location.href="#" + id;
+  // Busca el elemento en la página actual
+  const targetElement = document.getElementById(id);
+  if (targetElement) {
+    // Si lo encuentra, realiza un scroll suave
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  } else {
+    // Si no se encuentra (por ejemplo, no estamos en la página index), redirige a la página principal con el hash
+    window.location.href = "index.html#" + id;
+  }
 }
 document.addEventListener("DOMContentLoaded", function () {
     fetch("../local/menu.json")
